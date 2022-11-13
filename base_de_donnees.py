066b97639ce9e6303d8af6ea9fbb02d4efff458d
SZPNR <94069984+SZPNR@users.noreply.github.com>
2022-11-13 16:51:43 +0100

    Update base_de_donnees.py

--- base_de_donnees.py
+++ base_de_donnees.py
@@ -29,11 +29,16 @@ from google.auth.transport.requests import Request
 import pickle
 import os.path
 import base64
-import email
 from bs4 import BeautifulSoup
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+import email
+from email import policy
+from email.parser import BytesParser
+import glob
+import os
+
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
   
@@ -57,7 +62,8 @@ def getEmails():
   
     service = build('gmail', 'v1', credentials=creds)
   
-    result = service.users().messages().list(userId='me').execute()
+    result = service.users().messages().list(userId='me', maxResults=600).execute()
+    print(result)
   
     messages = result.get('messages')
 
@@ -84,11 +90,11 @@ def getEmails():
             soup = BeautifulSoup(decoded_data , "lxml")
             body = soup.body()
   
-            print("Subject: ", subject)
-            print("From: ", sender)
-            print("Message: ", body)
-            print('\n')
-            print("---------------------------------------------------------------------------")
+            #print("Subject: ", subject)
+            #print("From: ", sender)
+            #print("Message: ", body)
+            #print('\n')
+            #print("---------------------------------------------------------------------------")
 
             data = {
                 "subject": subject,
@@ -104,7 +110,10 @@ def getEmails():
 
         except:
             pass
-    
+    print(df_mails.dtypes)
+    df_mails["body"] = df_mails["body"].astype(str)
+    df_mails = df_mails.groupby('subject').agg(' '.join)
+    print(df_mails)
     return df_mails
   
   
@@ -115,14 +124,25 @@ def clean_data(df):
     brut = brut.split(" ")
     word = 0
     while word <= len(brut)-1:
-        print(str(word) + "   " + str(len(brut)))
         if "<p>" in brut[word]:
             brut[word] = brut[word].replace('<p>','')
-        elif (brut[word].startswith('(h')
+        if '\\' in brut[word]:
+            brut[word] = brut[word].replace('\\',' ')
+        if '>' in brut[word]:
+            brut[word] = brut[word].replace('>', '')
+        if '=' in brut[word]:
+            brut[word] = brut[word].replace('=', '')
+        if (brut[word].startswith('(h')
             or brut[word].startswith(')')
             or brut[word].startswith('\\')
             or brut[word].startswith(' ')
             or brut[word].startswith('[h')
+            or brut[word].startswith('"[h')
+            or brut[word].startswith('--')
+            or brut[word].endswith('--')
+            or brut[word].startswith('__')
+            or brut[word].endswith('__')
+            or '   ' in brut[word]
             or 'http' in brut[word]
             or 'https' in brut[word]
             or brut[word].endswith(')')
@@ -134,8 +154,33 @@ def clean_data(df):
     return df_mails
 
 
+def get_ham(len_df_mails):
+    path = 'ham/' # set this to "./" if in current directory
+    df_mails = pd.DataFrame(columns = ["body"])
+    eml_files = glob.glob(path + '*.eml.txt') # get all .eml files in a 
+    for eml_file in eml_files:
+        with open(eml_file) as email_file:
+            email_message = email.message_from_file(email_file).as_string()
+        email_file.close()
+        email_message = email_message.split("\n")
+        email_message = " ".join(email_message)
+        df_mail = pd.DataFrame([email_message], columns = ["body"])
+        df_mails = df_mails.append(df_mail, ignore_index=True)
+
+
+    return df_mails.head(len_df_mails)
+
 
 df_mails = getEmails()
 df_mails = pd.DataFrame(df_mails["body"].apply(clean_data))
 df_mails = df_mails.astype(str)
+df_mails['Spam'] = True
 df_mails.to_parquet("mails_spam_raw.parquet")
+len_df_mails = len(df_mails)
+
+df_ham = get_ham(len_df_mails)
+df_ham = pd.DataFrame(df_ham["body"].apply(clean_data))
+df_ham['Spam'] = False
+df_final = pd.concat([df_ham, df_mails], ignore_index=True)
+
+df_final.to_parquet("mails_spam_clean.parquet")
