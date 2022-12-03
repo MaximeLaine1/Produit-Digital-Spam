7249e616877bfc6e40d9920fb3f93d6d4bb8c808
SZPNR <94069984+SZPNR@users.noreply.github.com>
2022-12-03 01:59:42 +0100

    Update base_de_donnees.py

--- base_de_donnees.py
+++ base_de_donnees.py
@@ -1,4 +1,3 @@
-
 # pour installer les packages rendez vous dans votre terminal et écrivez:
 
 # pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
@@ -7,7 +6,7 @@
 # pip install pandas
 
 """
--Créez un nouveau dossier (votre bureau par exemple et foutez y ce programme)
+-Créez un nouveau dossier (votre bureau par exemple et mettez y ce programme)
 -Récupérez le fichier credentials.json et mettez le dans le dossier où vous avez ce programme
 -La première fois que vous allez lancer le programme il va automatiquement vous envoyer vers une page web
 -Connectez vous avec l'adresse spam.sep.2022.2023@gmail.com (mdp: hafhoumaxsim) et faites ce qui est écrit
@@ -28,6 +27,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import pickle
 import os.path
+from os import path
 import base64
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -38,6 +38,8 @@ from email import policy
 from email.parser import BytesParser
 import glob
 import os
+import unittest
+from unittest import mock
 
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -61,9 +63,10 @@ def getEmails():
             pickle.dump(creds, token)
   
     service = build('gmail', 'v1', credentials=creds)
-  
-    result = service.users().messages().list(userId='me', maxResults=600).execute()
-    print(result)
+
+    count = count_mail(service)
+    result = service.users().messages().list(userId='me', maxResults=count).execute()
+    
   
     messages = result.get('messages')
 
@@ -89,12 +92,6 @@ def getEmails():
   
             soup = BeautifulSoup(decoded_data , "lxml")
             body = soup.body()
-  
-            #print("Subject: ", subject)
-            #print("From: ", sender)
-            #print("Message: ", body)
-            #print('\n')
-            #print("---------------------------------------------------------------------------")
 
             data = {
                 "subject": subject,
@@ -110,20 +107,36 @@ def getEmails():
 
         except:
             pass
-    print(df_mails.dtypes)
+
     df_mails["body"] = df_mails["body"].astype(str)
     df_mails = df_mails.groupby('subject').agg(' '.join)
-    print(df_mails)
-    return df_mails
+
+    return df_mails["body"]
+
+
+def count_mail(service):
+
+    getProfile = service.users().getProfile(userId='me').execute()
+    total_mails = getProfile["messagesTotal"]
+
+    if not path.exists('mails_spam_clean.parquet'):
+        return 500
+    else:
+        df = pd.read_parquet('mails_spam_clean.parquet')
+        return int(total_mails - len(df.index)//2)
+    
   
   
 def clean_data(df):
+
     row = str(df)
     brut = row.replace('\r\n\r\n',' ')
     brut = brut.replace('\r\n',' ')
     brut = brut.split(" ")
     word = 0
+
     while word <= len(brut)-1:
+
         if "<p>" in brut[word]:
             brut[word] = brut[word].replace('<p>','')
         if '\\' in brut[word]:
@@ -139,6 +152,7 @@ def clean_data(df):
             or brut[word].startswith('[h')
             or brut[word].startswith('"[h')
             or brut[word].startswith('--')
+            or brut[word].startswith('</')
             or brut[word].endswith('--')
             or brut[word].startswith('__')
             or brut[word].endswith('__')
@@ -148,17 +162,22 @@ def clean_data(df):
             or brut[word].endswith(')')
             or brut[word].endswith('>')):
             brut.remove(brut[word])
+
         word += 1
-        
+
     df_mails = " ".join(brut)
+
     return df_mails
 
 
 def get_ham(len_df_mails):
-    path = 'ham/' # set this to "./" if in current directory
+
+    path = 'ham/' 
     df_mails = pd.DataFrame(columns = ["body"])
-    eml_files = glob.glob(path + '*.eml.txt') # get all .eml files in a 
+    eml_files = glob.glob(path + '*.eml.txt')
+
     for eml_file in eml_files:
+
         with open(eml_file) as email_file:
             email_message = email.message_from_file(email_file).as_string()
         email_file.close()
@@ -167,20 +186,44 @@ def get_ham(len_df_mails):
         df_mail = pd.DataFrame([email_message], columns = ["body"])
         df_mails = df_mails.append(df_mail, ignore_index=True)
 
-
     return df_mails.head(len_df_mails)
 
+def main():
 
-df_mails = getEmails()
-df_mails = pd.DataFrame(df_mails["body"].apply(clean_data))
-df_mails = df_mails.astype(str)
-df_mails['Spam'] = True
-df_mails.to_parquet("mails_spam_raw.parquet")
-len_df_mails = len(df_mails)
+    df_mails = getEmails()
+    df_mails = pd.DataFrame(df_mails.apply(clean_data))
+    df_mails = df_mails.astype(str)
+    df_mails['Spam'] = True
+    df_mails.to_parquet("mails_spam_raw.parquet")
 
-df_ham = get_ham(len_df_mails)
-df_ham = pd.DataFrame(df_ham["body"].apply(clean_data))
-df_ham['Spam'] = False
-df_final = pd.concat([df_ham, df_mails], ignore_index=True)
+    if path.exists('mails_spam_raw.parquet'):
+        df = pd.read_parquet('mails_spam_raw.parquet')
+        df_mails = pd.concat([df_mails, df], ignore_index=True)
+    len_df_mails = len(df_mails)
 
-df_final.to_parquet("mails_spam_clean.parquet")
+    df_ham = get_ham(len_df_mails)
+    df_ham = pd.DataFrame(df_ham["body"].apply(clean_data))
+    df_ham['Spam'] = False
+    df_final = pd.concat([df_ham, df_mails], ignore_index=True)
+
+    df_final.to_parquet("mails_spam_clean.parquet")
+
+def test_clean_data(df):
+
+    result = clean_data(df)
+    assert len(result) > 0
+
+def test_count_mail(service):
+
+    result = count_mail(service)
+    assert result.isnumeric()
+
+def test_get_ham(df):
+
+    result = get_ham(df)
+    assert len(result) > 0
+
+if __name__ == "__main__":
+    
+    main()
+    unittest.main()
